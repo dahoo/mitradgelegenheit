6c418c44501570e39f332d46ea2fe4f7d3346f22
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2014-11-01 23:25:15 +0100

    Add index for track_points

--- db/migrate/20141030174337_create_track_points.rb
+++ db/migrate/20141030174337_create_track_points.rb
@@ -1,7 +1,7 @@
 class CreateTrackPoints < ActiveRecord::Migration
   def change
     create_table :track_points do |t|
-      t.belongs_to :track
+      t.belongs_to :track, index: true
       t.float :latitude
       t.float :longitude
       t.integer :index
