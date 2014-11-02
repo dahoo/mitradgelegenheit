bf98c63098c64159c78248f0a89dfb8f005b2266
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2014-11-02 01:49:37 +0100

    Validate presence of track's name and time

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,8 @@ class Track < ActiveRecord::Base
   has_many :starts, dependent: :destroy
   has_many :ends, dependent: :destroy
 
+  validates :name, :time, presence: true
+
   def points_list
     self.track_points.map{ |p| [p.latitude, p.longitude] }
   end
