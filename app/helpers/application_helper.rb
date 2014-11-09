9c783d949a0085238343d1c567c351433d20d2de
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2014-11-09 01:54:41 +0100

    Add and style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def javascript(*files)
     content_for(:head) { javascript_include_tag(*files) }
   end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type.to_sym)} fade in") do
+          concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
