5d500fc6feb673d8116a4a524a309b7c6df79ac8
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-02-28 17:02:22 +0100

    Delayed evaluation of factory atrribute

--- spec/factories/start_times.rb
+++ spec/factories/start_times.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :start_time do
     day_of_week 1
-	time "2014-12-14 00:44:29"
-	track nil
+  	time "2014-12-14 00:44:29"
+  	track nil
   end
 end
--- spec/factories/track.rb
+++ spec/factories/track.rb
@@ -1,31 +1,37 @@
 FactoryGirl.define do
   factory :track do
     name 'Mitradgelegenheit'
-    start_times FactoryGirl.create_list(:start_time, 3)
-    track_points {[FactoryGirl.create(:track_point),
-                   FactoryGirl.create(:track_point_2),
-                   FactoryGirl.create(:track_point_3)]}
-    starts {[FactoryGirl.create(:start)]}
-    ends   {[FactoryGirl.create(:end)]}
+    start_times { FactoryGirl.create_list(:start_time, 3) }
+    track_points do
+      [FactoryGirl.create(:track_point),
+       FactoryGirl.create(:track_point_2),
+       FactoryGirl.create(:track_point_3)]
+    end
+    starts { [FactoryGirl.create(:start)] }
+    ends   { [FactoryGirl.create(:end)] }
 
     factory :track_attributes do
       name 'Mitradgelegenheit'
       time 'montags 8:00'
-      track_points {[FactoryGirl.create(:track_point),
-                     FactoryGirl.create(:track_point_2),
-                     FactoryGirl.create(:track_point_3)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
-      starts {[FactoryGirl.create(:start)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
-      ends   {[FactoryGirl.create(:end)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
+      track_points do
+        [FactoryGirl.create(:track_point),
+         FactoryGirl.create(:track_point_2),
+         FactoryGirl.create(:track_point_3)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';')
+      end
+      starts { [FactoryGirl.create(:start)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';') }
+      ends   { [FactoryGirl.create(:end)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';') }
     end
 
     factory :new_track_attributes do
       name 'Mitradgelegenheit 2'
       time 'freitags 8:00'
-      track_points {[FactoryGirl.create(:track_point),
-                     FactoryGirl.create(:track_point_2),
-                     FactoryGirl.create(:track_point_3)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
-      starts {[FactoryGirl.create(:start)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
-      ends   {[FactoryGirl.create(:end)].map{ |tp| "#{tp.latitude},#{tp.longitude}" }.join(';')}
+      track_points do
+        [FactoryGirl.create(:track_point),
+         FactoryGirl.create(:track_point_2),
+         FactoryGirl.create(:track_point_3)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';')
+      end
+      starts { [FactoryGirl.create(:start)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';') }
+      ends   { [FactoryGirl.create(:end)].map {|tp| "#{tp.latitude},#{tp.longitude}" }.join(';') }
     end
   end
 end
