bdbd9be29ea5b79c6548a4bd7abd89bb9289e587
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-07-10 01:05:15 +0200

    Let users set name, remove default name

--- db/migrate/20150708202700_rename_admins_to_users.rb
+++ db/migrate/20150708202700_rename_admins_to_users.rb
@@ -2,6 +2,6 @@ class RenameAdminsToUsers < ActiveRecord::Migration
   def change
     rename_table :admins, :users
     add_column :users, :admin, :boolean, default: false, null: false
-    add_column :users, :name, :string, default: 'User', null: false
+    add_column :users, :name, :string, null: false
   end
 end
