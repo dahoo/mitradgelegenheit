43aa83117a8ab9aee8def4743339f79db9b8570a
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-09-17 17:55:19 +0200

    Add devise recoverable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable,
+         :recoverable
 
   validates :name, presence: true, allow_blank: false
 end
