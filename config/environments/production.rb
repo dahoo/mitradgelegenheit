5f2eeb4cf0552b51fde8f50fec62355ce77e8e6a
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-09-22 23:20:08 +0200

    Use sendmail instead of smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'mitradg.fomalhaut.uberspace.de' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "fomalhaut.uberspace.de",
-    :port                 => 587,
-    :user_name            => ENV['MAIL_USERNAME'],
-    :password             => ENV['MAIL_PASSWORD'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :sendmail
 end
