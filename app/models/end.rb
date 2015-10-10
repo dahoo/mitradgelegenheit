540baf9beb9641790e813ae124bcd4071168266d
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-10-10 15:45:43 +0200

    Display correct index in edit view

--- app/models/end.rb
+++ app/models/end.rb
@@ -1,2 +1,5 @@
 class End < WayPoint
+  def index
+    track.ends.index(self).try(:+, 1) if track
+  end
 end
--- app/models/start.rb
+++ app/models/start.rb
@@ -1,2 +1,5 @@
 class Start < WayPoint
+  def index
+    track.starts.index(self).try(:+, 1) if track
+  end
 end
